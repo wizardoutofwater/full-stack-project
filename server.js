850d9aff892e171669b8aa72d033b5dcf36616cc
Mike McDonald <whoiswizard@icloud.com>
2021-02-27 10:32:43 -0600

    added handlebars structure, installed express handlebars updated server.js

--- server.js
+++ server.js
@@ -1,49 +1,68 @@
 const express = require("express");
 const app = express();
 const db = require("./models");
+const crypto = require("crypto");
+const pbkdf2 = require("pbkdf2");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+
+// Set Handlebars as view engine
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: "hbs",
+    defaultLayout: "default",
+    partialsDir: "/views/partials/",
+  })
+);
+// Serve Static css/image files
+app.use(express.static("public"));
 
 
-  app.get("/api", function(request, response, next){
-    console.log("someone sent a request home");
-    response.send();
-});
-
-app.get("/api/search", function(request, response, next){
+// -----Routes-----
+app.get("/api", function (request, response, next) {
   console.log("someone sent a request home");
   response.send();
 });
 
-app.get("/api/login", function(request, response, next){
+app.get("/api/search", function (request, response, next) {
   console.log("someone sent a request home");
   response.send();
 });
 
-app.get("/api/school", function(request, response, next){
-  console.log("someone sent a request home");
-  response.send();
-});
-  app.get("/api/school/:id", function(request, response, next){
-    console.log("someone sent a request home");
-    response.send();
-});
-app.post("/api/login", function(request, response, next){
+app.get("/api/login", function (request, response, next) {
   console.log("someone sent a request home");
   response.send();
 });
 
-app.post("/api/alumni", function(request, response, next){
+app.get("/api/school", function (request, response, next) {
+  console.log("someone sent a request home");
+  response.send();
+});
+app.get("/api/school/:id", function (request, response, next) {
+  console.log("someone sent a request home");
+  response.send();
+});
+app.post("/api/login", function (request, response, next) {
   console.log("someone sent a request home");
   response.send();
 });
 
-app.post("/api/school", function(request, response, next){
+app.post("/api/alumni", function (request, response, next) {
   console.log("someone sent a request home");
   response.send();
 });
 
-app.listen(3000,function(){
-    console.log("listening in port 3000");
-  });
+app.post("/api/school", function (request, response, next) {
+  console.log("someone sent a request home");
+  response.send();
+});
+
+app.listen(3000, function () {
+  console.log("listening in port 3000");
+});
